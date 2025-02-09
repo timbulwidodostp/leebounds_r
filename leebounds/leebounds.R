2f0eecacc8ce479d861c690f3ce285c41603552c
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2025-02-09 19:16:54 +0700

    Use leebounds With (In) R Software

---
+++ leebounds/leebounds.R
@@ -0,0 +1,16 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Nonparametric bounds on Average Treatment Effect in the presence of Endogenous Selection Use leebounds With (In) R Software
+install.packages("remotes")
+remotes::install_github("haluong89-bcn/leebounds")
+library("leebounds")
+leebounds = read.csv("https://raw.githubusercontent.com/timbulwidodostp/leebounds_r/main/leebounds/leebounds.csv",sep = ";")
+# Estimation Nonparametric bounds on Average Treatment Effect in the presence of Endogenous Selection Use leebounds With (In) R Software
+leebounds(leebounds)
+GetBounds(leebounds(leebounds))
+# Nonparametric bounds on Average Treatment Effect in the presence of Endogenous Selection Use leebounds With (In) R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
